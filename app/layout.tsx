becc03a74024980a7a67b7bf28340bc51a43753c
LEVIII007 <shashanktyagiji12345@gmail.com>
2026-01-24 20:27:22 +0530

    [Shashank] new widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
           </ThemeProvider>
         </GoogleOAuthProvider>
         <Script
-          src=" https://rle3ob7wdla6y74q.public.blob.vercel-storage.com/conversly/loader.min-tAhPLQoyyvqQzXv2MivedyoIfubZQW.js"
+          src="https://rle3ob7wdla6y74q.public.blob.vercel-storage.com/conversly/loader.min-jwb9YLnmpKqsNxuTn63adNcmveGvv4.js"
           data-chatbot-id="a7zzrm5gpn4pax804olp52tp"
           data-testing="false"
           strategy="afterInteractive"
